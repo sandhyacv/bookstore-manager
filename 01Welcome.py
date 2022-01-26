19f30851d0c294f8a9a1ddbfc69f9f282b42ef4a
Sandhya C Vijay <97396685+sandhyacv@users.noreply.github.com>
2022-01-26 16:48:56 +0530

    added files via upload (welcome page).

---
+++ 01Welcome.py
@@ -0,0 +1,44 @@
+from tkinter import *
+from tkinter import messagebox
+
+def goodBye():
+    messagebox.showinfo(message="Thank You for Using BMS", title="Bookstore Manager")
+    root.quit()
+
+def asAdmin():
+    return
+
+def asCustomer():
+    return
+
+root = Tk()
+root.title("Bookstore Management System")
+root.geometry('960x540')
+root.iconbitmap(r"BookstoreMS\bms.ico")
+root.attributes("-alpha", 0.95)
+root.wm_protocol("WM_DELETE_WINDOW", goodBye)
+root.resizable(FALSE,FALSE)
+
+mainframe = Frame(root, bg="#FFFFFF")
+mainframe.grid(column=0, row=0)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+framebg1 = PhotoImage(file=r"BookstoreMS\welcometitle.png")
+bglabel1 = Label(mainframe, image = framebg1, bg="#FFFFFF")
+
+framebg2 = PhotoImage(file=r"BookstoreMS\welcomecloud.png")
+bglabel2 = Label(mainframe, image = framebg2, bg="#FFFFFF")
+
+login = Label(mainframe, text = "Login As     ", font = ("Berlin Sans FB", 24), bg="#FFFFFF", fg="#2e2e2e")
+
+logAdmin = Button(mainframe, text = "Admin", pady = 5, width = 20, bg="#2e2e2e", fg="#FFFFFF", font = ("Berlin Sans FB", 10), bd=2, cursor="hand2", relief="groove", command = asAdmin)
+logCustomer = Button(mainframe, text = "Customer", pady = 5, width = 20, bg="#2e2e2e", fg="#FFFFFF", font = ("Berlin Sans FB", 10), bd=2, cursor="hand2", relief="groove", command = asCustomer)
+
+bglabel1.grid(row=0, column=0, columnspan=8)
+bglabel2.grid(row=0, column=8, rowspan=6)
+login.grid(row=1, column=0)
+logAdmin.grid(row=2, column=0)
+logCustomer.grid(row=3, column=0)
+
+root.mainloop()
