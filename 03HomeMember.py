bccd800f56c781fcb734cbb3c19574dfe3c356aa
Sandhya C Vijay <97396685+sandhyacv@users.noreply.github.com>
2022-01-27 13:29:58 +0530

    updated 03HomeAdmin.py (configure).

--- 03HomeMember.py
+++ 03HomeMember.py
@@ -40,7 +40,7 @@ root.rowconfigure(0, weight=1)
 
 titleImg = PhotoImage(file = r"BookstoreMS\titlestrip.png").subsample(2,2)
 titleLbl = Label(mainframe, image = titleImg, bg="#FFFFFF")
-logoutBut = Button(mainframe, text = "LOGOUT", width = 12, bg = "#41404A", fg = "#FFFFFF", font = ("Berlin Sans FB", 14), relief = "flat", command = loggedOut)
+logoutBut = Button(mainframe, text = "LOGOUT", width = 12, bg = "#41404A", fg = "#FFFFFF", font = ("Berlin Sans FB", 14), relief = "flat", cursor="hand2", command = loggedOut)
 
 bookReservedBut = Button(mainframe, text = "Reserve Book", width=18, bg="#9e9e9e", fg="#2e5170", font = ("Berlin Sans FB", 14), cursor="hand2", relief="groove", command = bookReserved)
 
